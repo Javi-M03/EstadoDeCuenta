b092b06051edd465da3b5fdeb54691a18d5bec06
Javier Martinez <javi.martinez097@gmail.com>
2026-09-05 21:03:27 -0600

    Creacion de AppDBContext.cs y de mis configurations para las migraciones de EF Se incorporo Entity Framewoek

---
+++ EstadoDeCuenta.Infrastructure/Data/AppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstadoDeCuenta.Domain.Entities;
+
+
+namespace EstadoDeCuenta.Infrastructure.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Movement> Movements { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDBContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EstadoDeCuenta.Infrastructure/Data/Configurations/ClientConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EstadoDeCuenta.Domain.Entities;
+
+namespace EstadoDeCuenta.Infrastructure.Data.Configurations
+{
+    public class ClientConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.HasKey(c => c.ClientId);
+            builder.Property(c => c.ClientName).IsRequired().HasMaxLength(256);
+            builder
+                .HasMany(c => c.Cards)
+                .WithOne(t => t.Client)
+                .HasForeignKey(t => t.ClientId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
